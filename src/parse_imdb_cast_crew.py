2d5f7a414253315d46bc5e22fae6bdcc0c03c6f2
kashifjt <kashifjt@gmail.com>
2014-09-21 16:58:46 -0700

    Top 3 Actors

--- src/parse_imdb_cast_crew.py
+++ src/parse_imdb_cast_crew.py
@@ -8,9 +8,12 @@ import re
 
 path = "html//imdb_pages//"
 data_reader = csv.reader(open("filtered_data.csv"), delimiter=',')
-data_writer = csv.writer(open("imdb_movie_crew.csv", "ab"))
+data_writer1 = csv.writer(open("imdb_movie_crew.csv", "ab"))
+data_writer2 = csv.writer(open("imdb_movie_actor.csv", "ab"))
 output = [("Movie Name","Type","Value")]
-data_writer.writerows(output)
+data_writer1.writerows(output)
+output = [("Movie Name","Actor")]
+data_writer2.writerows(output)
 
 next(data_reader)
 
@@ -29,18 +32,22 @@ for row in data_reader:
         directors=top_div.xpath('*[@itemprop="director"]/a/span/text()')
         for i in range(0,len(directors)):
                 output = [(movie_title,"Director",directors[i])]
-                data_writer.writerows(output)
+                data_writer1.writerows(output)
         writers=top_div.xpath('*[@itemprop="creator"]/a/span/text()')
         for i in range(0,len(writers)):
                 output = [(movie_title,"Screenwriter",writers[i])]
-                data_writer.writerows(output)
+                data_writer1.writerows(output)
+        actors=top_div.xpath('*[@itemprop="actors"]/a/span/text()')
+        for i in range(0,len(actors)):
+                output = [(movie_title,actors[i])]
+                data_writer2.writerows(output)
                 
         movie_details=html_page.xpath('//*[@id="titleDetails"]')
         if len(movie_details) !=0:
             prod_cos=movie_details[0].find_class("itemprop")
             if len(prod_cos) !=0:
                 output = [(movie_title,"Producer",prod_cos[0].text_content())]
-                data_writer.writerows(output)
+                data_writer1.writerows(output)
     
     #if counter==10:
     #    break

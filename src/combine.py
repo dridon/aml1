72e15100c044b39a76165baefc4f3d650acf0d06
Faiz Khan <dridon@gmail.com>
2014-09-20 20:17:37 -0400

    Raw data set

--- src/combine.py
+++ src/combine.py
@@ -8,21 +8,112 @@ def seek(f, csv, line):
 moviesf =  open("filtered_data.csv", "r")
 actorsf =  open("actor_data.csv", "r")
 tcrewf  =  open("technicalCrew.csv", "r")
+tomatorf = open("movie_ratings.csv", "r")
+imdbrf = open("imdb_movie_rating.csv", "r")
 
 moviesc = csv.reader(moviesf)
 actorsc = csv.reader(actorsf)
 tcrewc = csv.reader(tcrewf)
+tomatorc = csv.reader(tomatorf)
+imdbrc = csv.reader(imdbrf)
 
+next(moviesc)
 next(actorsc)
 next(tcrewc) 
+next(tomatorc)
+next(imdbrc)
 
-actors = { a[1] for a in actorsc}
-directors = { t[2] for t in tcrewc if t[1] == "Director" }
-
+actors = { a[1].strip() for a in actorsc}
+seek(actorsf, actorsc, 1)
+directors = { t[2].strip() for t in tcrewc if t[1] == "Director" }
 seek(tcrewf, tcrewc, 1)
-producers = { t[2] for t in tcrewc if t[1] == "Producer" }
-
+producers = { t[2].strip() for t in tcrewc if t[1] == "Producer" }
 seek(tcrewf, tcrewc, 1)
-swriter = { t[2] for t in tcrewc if t[1] == "Screenwriter" }
+swriter = { t[2].strip() for t in tcrewc if t[1] == "Screenwriter" }
+seek(tcrewf, tcrewc, 1)
+movies = { t[0].strip() for t in moviesc } 
+seek(moviesf, moviesc, 1)
 
+def csv_dict(csv, f): 
+  d = {} 
+  for row in csv:
+    d[row[0].strip()] = row 
 
+  f.seek(0)
+  next(csv)
+  return d 
+
+def load_dic_list(d, k, v): 
+  k = k.strip()
+  if not k in d: 
+    d[k] = [] 
+  d[k].append(v) 
+
+movied = csv_dict(moviesc, moviesf) 
+tomatord = csv_dict(tomatorc, tomatorf)  
+imdbrd = csv_dict(imdbrc, imdbrf) 
+
+actorsd = {} 
+directord = {}
+producerd = {}
+swriterd = {}
+
+for a in actorsc: 
+  load_dic_list(actorsd, a[0], a[1])
+
+for t in tcrewc:
+  if t[1] == "Director": 
+    load_dic_list(directord, t[0], t[2])
+  elif t[1] == "Producer": 
+    load_dic_list(producerd, t[0], t[2])
+  elif t[1] == "Screenwriter": 
+    load_dic_list(swriterd, t[0], t[2])
+
+ds = [movied, tomatord, imdbrd, actorsd, directord, producerd, swriterd]
+
+def key_in_all(k, ds):
+  in_all = True
+
+  for d in ds:
+    if not k in d: 
+      in_all = False 
+
+  return in_all
+
+raw_data = [("Movie Name", "Release Date", "Genre", "Budget", "Gross", "RT Critic Label", "RT Critic Rating", "RT Audience Label", "RT Audience Rating", "IMDB Rating", "Directors", "Producers", "Sreenwriters", "Actors")]
+data = [("Movie Name", "Hot Season", "Budget", "Director RT Average", "Director RT Public Average", "Director IMDB Average", "Producer RT Critical Average", "Producer RT Public Average", "Producer IMDB Average", "Screenwriter RT Critical Average", "Screenwriter RT Public Average", "Screenwriter IMDB Average","Actors RT Critical Average", "Actors RT Public Average", "Actors IMDB Average", "Gross")]
+
+for k in movied.iterkeys(): 
+  j = j + 1 
+  if key_in_all(k, ds):
+    i = i + 1
+    raw_data.append((
+      movied[k][0], 
+      movied[k][1], 
+      movied[k][2], 
+      movied[k][3], 
+      movied[k][4], 
+      tomatord[k][1], 
+      tomatord[k][2], 
+      tomatord[k][3], 
+      tomatord[k][4],
+      imdbrd[k][1],
+      "|".join(directord[k]),
+      "|".join(producerd[k]),
+      "|".join(swriterd[k]),
+      "|".join(actorsd[k]),
+    ))
+
+actors_writer = csv.writer(open("actors.csv", "w+"))
+directors_writer = csv.writer(open("directors.csv", "w+"))
+producers_writer = csv.writer(open("producers.csv", "w+"))
+screenwriter_writer = csv.writer(open("screenwriter.csv", "w+"))
+raw_writer = csv.writer(open("full_raw_features.csv", "w+"))
+data_writer = csv.writer(open("feature_data.csv", "w+"))
+
+actors_writer.writerows(actors) 
+directors_writer.writerows(directors)
+producers_writer.writerows(producers)
+screenwriter_writer.writerows(swriter)
+raw_writer.writerows(raw_data)
+data_writer.writerows(data)

f76a50d2e14288e57d8861e29150253906b5aff0
kashifjt <kashifjt@gmail.com>
2014-09-20 17:11:03 -0700

    IMDB User Rating

--- src/parse_imdb_ratings.py
+++ src/parse_imdb_ratings.py
@@ -9,7 +9,7 @@ import re
 path = "html//imdb_pages//"
 data_reader = csv.reader(open("filtered_data.csv"), delimiter=',')
 data_writer = csv.writer(open("imdb_movie_rating.csv", "ab"))
-output = [("Movie Name", "IMDB Ratings", "Metascore")]
+output = [("Movie Name", "IMDB Rating")]
 data_writer.writerows(output)
 
 next(data_reader)
@@ -18,20 +18,21 @@ counter=0
 for row in data_reader: 
     movie_title=row[0]
     user_rating=''
-    metascore=''
     html_path=path+re.sub(r"[:|?|']","",movie_title)+".html"
     if isfile(html_path):
+        print movie_title
         html_page=lxml.html.document_fromstring(open(html_path,"r").read())
         try:
             user_rating=html_page.xpath('//*[@id="overview-top"]/div[3]/div[3]/strong/span/text()')[0]
         except Exception:
             user_rating=''
-        try:
-            metascore=html_page.xpath('//*[@id="overview-top"]/div[3]/div[3]/a[2]/text()')[0].strip().split('/')[0]
-        except Exception:
-            metascore=''
-    output=[(movie_title,user_rating,metascore)]
-    data_writer.writerows(output)
+    #    try:
+    #        metascore=html_page.xpath('//*[@id="overview-top"]/div[3]/div[3]/a[2]/text()')[0].strip().split('/')[0]
+    #    except Exception:
+    #        metascore=''
+        if user_rating!='':
+            output=[(movie_title,user_rating)]
+            data_writer.writerows(output)
     #counter=counter+1
     #if counter==10:
     #    break

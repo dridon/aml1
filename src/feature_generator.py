b3571412ace70e524cecf8ca9c00dd9e8d823b8a
Faiz Khan <dridon@gmail.com>
2014-09-21 20:10:34 -0400

    Mean Ratings for People

--- src/feature_generator.py
+++ src/feature_generator.py
@@ -3,7 +3,6 @@ import time
 import numpy as np 
 import scipy.stats.mstats as ms
 
-
 raw_dataf = open("data/features/full_raw_features.csv", "r")
 raw_datac = csv.reader(raw_dataf)
 
@@ -72,6 +71,13 @@ next(raw_datac)
 raw_data = csv_dict(raw_datac, raw_dataf)
 directord, producerd, swriterd, actord = people_dicts(raw_data)
 
+def dict2list(d):
+  l = [] 
+  for k in d.iterkeys():
+    v = (k, ms.gmean(d[k][0]), ms.gmean(d[k][1]), ms.gmean(d[k][2]))
+    l.append(v)
+  return l
+
 def ratings(d, ks, rating_index):
   avgs = [] 
   for k in ks:
@@ -109,5 +115,20 @@ def get_data(raw_data, directord, producerd, swriterd, actord):
 
 data = get_data(raw_data, directord, producerd, swriterd, actord) 
 dataw= [("Hot Season", "Budget", "Director RT Critical Average", "Director RT Public Average", "Director IMDB Average", "Producer RT Critical Average", "Producer RT Public Average", "Producer IMDB Average", "Screenwriter RT Critical Average", "Screenwriter RT Public Average", "Screenwriter IMDB Average","Actors RT Critical Average", "Actors RT Public Average", "Actors IMDB Average", "Gross")] + data
+
+directors = [("Name", "RTC Mean", "RTA Mean", "IMDB Mean")] + dict2list(directord) 
+producers = [("Name", "RTC Mean", "RTA Mean", "IMDB Mean")] + dict2list(producerd)
+swriters = [("Name", "RTC Mean", "RTA Mean", "IMDB Mean")] + dict2list(swriterd)
+actors = [("Name", "RTC Mean", "RTA Mean", "IMDB Mean")] + dict2list(actord)
+
+director_writer = csv.writer(open("data/features/director_data.csv", "w+"))
+producer_writer = csv.writer(open("data/features/producer_data.csv", "w+"))
+swriter_writer = csv.writer(open("data/features/swriter_data.csv", "w+"))
+actor_writer = csv.writer(open("data/features/actor_data.csv", "w+"))
 data_writer = csv.writer(open("data/features/feature_data.csv", "w+"))
+
+directors_writer.writerows(directors)
+producer_writer.writerows(producers)
+swriter_writer.writerows(swriters)
+actor_writer.writerows(actors)
 data_writer.writerows(dataw)
